6e8653b331ff9fa5b51fdbc125fe0965410f9b1b
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2018-07-01 20:37:37 +0200

    final

--- Assignment2/tileBlur.m
+++ Assignment2/tileBlur.m
@@ -2,20 +2,132 @@ function tileBlur()
 
 clc;
 clear all;
+%% Reading Input Image
 
 I = imread('cameraman.tif');
-
 if numel(size(I)) >= 3
     I = rgb2gray(I);
 end
 
-G = double(I);
+figure(1)
+subplot(2,3,1)
+imshow(I,[])
+title('Original Image')
+
+M = size(I,1);
+N = size(I,2);
+
+pSig1 = max(max(I(1:floor(0.1*M),:)));
+pSig2 = max(max(I(size(I,1)-floor(0.1*M):size(I,1),:)));
+pSig3 = max(max(I(:,size(I,2)-floor(0.1*N):size(I,2))));
+pSig4 = max(max(I(:,1:floor(0.1*N))));
+pSig = max([pSig1 pSig2 pSig3 pSig4]);
+
+numIt = 20;
+
+%% Adding blur to the input Image
+
+sigmaBlur = 1;
+len = 10;
+
+PSF = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 1 1 0 0 0 1 1 1 1 1 0 0 0 0;
+    0 0 0 0 0 1 1 1 0 0 0 1 1 1 1 0 0 0 0 0;
+    0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 0 0 0 0 0;
+    0 0 0 0 0 0 1 1 1 1 0 0 1 1 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 0;
+    0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0;
+    0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+    ];
+PSF = PSF*0.01;
+% PSF = fspecial('gaussian', len, sigmaBlur); %Uniform gaussian blur for experiment
+G = imfilter(I, PSF, 'conv');
+figure(1)
+subplot(2,3,2)
+imshow(G,[])
+title('Result of convolution with non-uniform PSF')
+
+%% Deconvolving the blurred Image without solving for Boundary Value Problem
+
+J1 = deconvlucy(G,PSF,numIt);
+figure(2)
+imshow(PSF,[])
+figure(1)
+subplot(2,3,4)
+imshow(J1,[])
+
+numElem = numel(J1(1:floor(0.1*M),:)) + numel(J1(:,1:floor(0.1*N))) + numel(J1(:,size(G,2)-floor(0.1*N):size(G,2))) + numel(J1(size(G,1)-floor(0.1*M):size(G,1),:)); 
+noiseSqr = sqrt(sum(sum((J1(1:floor(0.1*M),:)-I(1:floor(0.1*M),:)).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J1(:,1:floor(0.1*N))-I(:,1:floor(0.1*N))).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J1(:,size(G,2)-floor(0.1*N):size(G,2))-I(:,size(G,2)-floor(0.1*N):size(G,2))).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J1(size(G,1)-floor(0.1*M):size(G,1),:)-I(size(G,1)-floor(0.1*M):size(G,1),:)).^2)));
+noiseSqr = noiseSqr / numElem;
+
+pSNR = db(double(pSig)/noiseSqr)
+hT1 = title('Restored Image without filling boundaries');
+T1Pos = round(get(hT1,'Position')); %// Get the position
+hT1_2 = text(T1Pos(1),T1Pos(2) + size(J1,2)+10,strcat('PSNR =', {' '}, num2str(pSNR,'%.2f'), 'dB'),'HorizontalAlignment','center'); %// Place the text
+
+
+%% Deconvolving after using edgetaper(.) command
+
+G = G(len+1:size(G,1)-len,len+1:size(G,2)-len);
+% G = G(1:size(G,1)-len,1:size(G,2)-len); %Cropping G matrix of the boundary values that need to be filled; 
+%VVV IMPORTANT!!!!!!!!!!!!!!!!!!!!! 
+%This cropping information is missing from the paper
+
 M = size(G,1);
 N = size(G,2);
+tic
+% timerVal = tic
+G = edgetaper(G,PSF);
+J2 = deconvlucy(G,PSF,numIt);
+toc
+% elapsedTime = toc
+% toc(timerVal)
+% elapsedTime = toc(timerVal)
+
+figure(1)
+subplot(2,3,5)
+imshow(J2,[])
+hT2 = title('Restored Image after using edgetaper(.)')
+T2Pos = round(get(hT2,'Position')); %// Get the position
+
+I = I(len+1:size(I,1)-len,len+1:size(I,2)-len);
+
+pSig1 = max(max(I(1:floor(0.1*M),:)));
+pSig2 = max(max(I(size(I,1)-floor(0.1*M):size(I,1),:)));
+pSig3 = max(max(I(:,size(I,2)-floor(0.1*N):size(I,2))));
+pSig4 = max(max(I(:,1:floor(0.1*N))));
+pSig = max([pSig1 pSig2 pSig3 pSig4]);
+numElem = numel(J2(1:floor(0.1*M),:)) + numel(J2(:,1:floor(0.1*N))) + numel(J2(:,size(G,2)-floor(0.1*N):size(G,2))) + numel(J2(size(G,1)-floor(0.1*M):size(G,1),:)); 
+noiseSqr = sqrt(sum(sum((J2(1:floor(0.1*M),:)-I(1:floor(0.1*M),:)).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J2(:,1:floor(0.1*N))-I(:,1:floor(0.1*N))).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J2(:,size(G,2)-floor(0.1*N):size(G,2))-I(:,size(G,2)-floor(0.1*N):size(G,2))).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J2(size(G,1)-floor(0.1*M):size(G,1),:)-I(size(G,1)-floor(0.1*M):size(G,1),:)).^2)));
+noiseSqr = noiseSqr / numElem;
+
+pSNREdgeTaper = db(double(pSig)/noiseSqr)
+hT2_2 = text(T2Pos(1),T2Pos(2) + size(J2,2)+10,strcat('PSNR =', {' '}, num2str(pSNREdgeTaper,'%.2f'), 'dB'),'HorizontalAlignment','center'); %// Place the text
+
+
+%% Solving for boundary value problem using tile generation method
+
+tic
 alpha = 10;
-sigma = 10;
-% h = fspecial('gaussian',60,sigma);
-% size(G)
+sigma = 2;
 Iconv = imgaussfilt(G,sigma);
 
 for i = 1:alpha
@@ -33,13 +145,6 @@ mask = Bprime < 1;
 mask = imfill(mask,'holes');
 Bprime = regionfill(Bprime, mask);
 
-
-% xA_co = [1 N N 1];
-% yA_ro = [alpha alpha M+alpha+1 M+alpha+1];
-% xB_co = [alpha alpha+N alpha+N alpha];
-% yB_ro = [1 1 M M];
-
-
 for i = 1:alpha
     Cprime(i,:) = Bprime(M-alpha+i,:);
     Cprime(M+alpha+i,:) = Bprime(i,:);
@@ -52,11 +157,6 @@ end
 mask = Cprime < 1;
 mask = imfill(mask,'holes');
 Cprime = regionfill(Cprime, mask);
-
-% xC_co = [alpha alpha+N alpha+N alpha];
-% yC_ro = [alpha alpha alpha+M alpha+M];
-% 
-% Cprime = regionfill(Cprime, xC_co, yC_ro);
 
 A = Aprime(alpha+1:alpha+M,1:N);
 B = Bprime(1:M,alpha+1:alpha+N);
@@ -72,17 +172,50 @@ bigIm(2*M+1:3*M,1:N) = C;
 bigIm(2*M+1:3*M,N+1:2*N) = A;
 bigIm(2*M+1:3*M,2*N+1:3*N) = C;
 
-
+vTile = bigIm(M/2+1:M/2+2*M,N/2+1:N/2+2*N);
 % 
-% figure(2)
-% subplot(3,1,1)
-% imshow(Aprime,[])
-% subplot(3,1,2)
-% imshow(Bprime,[])
-% subplot(3,1,3)
-% imshow(Cprime,[])
+% % 
+% % figure(2)
+% % subplot(3,1,1)
+% % imshow(Aprime,[])
+% % subplot(3,1,2)
+% % imshow(Bprime,[])
+% % subplot(3,1,3)
+% % imshow(Cprime,[])
+% % 
+% % figure(2)
+% % imshow(vTile,[])
 % 
+% J = deconvlucy(G,PSF);
 % figure(3)
-% imshow(bigIm,[])
+% imshow(J,[])
+%
+figure(3)
+imshow(vTile,[])
+J = deconvlucy(vTile,PSF,numIt);
+J3 = J(M/2+1:M/2+M,N/2+1:N/2+N);
+toc
+figure(1)
+subplot(2,3,6)
+imshow(J3,[])
+title('Restored Image after using tile method')
+hT3 = title('Restored Image after using tile method')
+T3Pos = round(get(hT3,'Position')); %// Get the position
+
+%% Numerical analysis
+
+
+
+
+numElem = numel(J3(1:floor(0.1*M),:)) + numel(J3(:,1:floor(0.1*N))) + numel(J3(:,size(I,2)-floor(0.1*N):size(I,2))) + numel(J3(size(I,1)-floor(0.1*M):size(I,1),:)); 
+noiseSqr = sqrt(sum(sum((J3(1:floor(0.1*M),:)-I(1:floor(0.1*M),:)).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J3(:,1:floor(0.1*N))-I(:,1:floor(0.1*N))).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J3(:,size(I,2)-floor(0.1*N):size(I,2))-I(:,size(I,2)-floor(0.1*N):size(I,2))).^2)));
+noiseSqr = noiseSqr + sqrt(sum(sum((J3(size(I,1)-floor(0.1*M):size(I,1),:)-I(size(I,1)-floor(0.1*M):size(I,1),:)).^2)));
+noiseSqr = noiseSqr / numElem;
+
+pSNRTileAlgo = db(double(pSig)/noiseSqr)
+hT3_2 = text(T3Pos(1),T3Pos(2) + size(J3,2)+10,strcat('PSNR =', {' '}, num2str(pSNRTileAlgo,'%.2f'), 'dB'),'HorizontalAlignment','center'); %// Place the text
+
 
 end
--- Codes to Submit/main.m
+++ Codes to Submit/main.m
@@ -3,51 +3,58 @@ function main()
 clear all;
 clc;
 
-I = imread('peppers.png');
+I = imread('thank.jpg');
 
 if numel(size(I)) >= 3
     I = rgb2gray(I);
 end
 
-numIte = 4;
+figure(5)
+imshow(I)
+numIte = 10;
 theta1 = 90;    %Horizontal Motion Blur
 theta2 = 0;     %Vertical Motion Blur
+lengthB = 50;
 
-[I1, I2, dBI1, dBI2] = Assignment_1(I, numIte, 40, theta1, theta2);
+[I1, I2, dBI1, dBI2] = Assignment_1(I, numIte, lengthB, theta1, theta2);
 
-figure()
-subplot(2,3,1)
-imshow(I)
-title('Original Image')  
-subplot(2,3,2)
+% figure()
+% subplot(2,3,1)
+% imshow(I)
+% title('Original Image')  
+% subplot(2,3,2)
+figure(1)
 imshow(I1)
-title('Image1 with Horizontal Motion Blur')  
-subplot(2,3,3)
+% title('Image1 with Horizontal Motion Blur')  
+% subplot(2,3,3)
+figure(2)
 imshow(I2)
-title('Image2 with Vertical Motion Blur')  
-subplot(2,3,5)
+% title('Image2 with Vertical Motion Blur')  
+% subplot(2,3,5)
+figure(3)
 imshow(dBI1,[])
-title(strcat('Restored Image1 after', {' '}, num2str(numIte), ' iterations'))  
-subplot(2,3,6)
+% title(strcat('Restored Image1 after', {' '}, num2str(numIte), ' iterations'))  
+% subplot(2,3,6)
+figure(4)
 imshow(dBI2,[])
-title(strcat('Restored Image2 after', {' '}, num2str(numIte), ' iterations'))  
+% title(strcat('Restored Image2 after', {' '}, num2str(numIte), ' iterations'))  
 
-theta1 = -90:5:90;
-theta2 = 0;
-for i = 1:numel(theta1)
-    [I1, I2, dBI1, dBI2] = Assignment_1(I, numIte, 40, theta1(i), theta2);
-    s1(i) = estimate_sharpness(dBI1);
-    s2(i) = estimate_sharpness(dBI2);
-    s3(i) = max([s1(i) s2(i)]);
-end
+% theta1 = -90:1:90;
+% theta2 = 0;
+% for i = 1:numel(theta1)
+%     [I1, I2, dBI1, dBI2] = Assignment_1(I, numIte, 40, theta1(i), theta2);
+%     s1(i) = estimate_sharpness(dBI1);
+%     s2(i) = estimate_sharpness(dBI2);
+%     s3(i) = max([s1(i) s2(i)]);
+% end
 
-figure()
-subplot(3,1,1)
-plot(theta1, s1)
-subplot(3,1,2)
-plot(theta1, s2)
-subplot(3,1,3)
-plot(theta1, s3)
+% figure()
+% % subplot(3,1,1)
+% plot(theta1, s1)
+% % subplot(3,1,2)
+% % plot(theta1, s2)
+% % subplot(3,1,3)
+% % plot(theta1, s3)
 
 
 
